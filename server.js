18ae706ffbddaa65c0fac63f7dc64ebe6cf48492
L352618 <l352618@KPAdmins-MacBook-Pro.local>
2017-12-02 20:11:20 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const request = require('request');
+
+const hbs = require('hbs');
+
+var app = express();
+
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname +'/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentTime: new Date().getFullYear()
+  });
+});
+  /*
+  //res.send("<h1>Hello Express</h1>");
+  res.send({
+    name: 'pyug',
+    likes: [
+      'basketball',
+      'science fiction'
+    ]
+  });
+  */
+
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs", {
+  pageTitle: 'Maintenance page'
+});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+    currentYear:  new Date().getFullYear()
+  });
+
+  //res.send('about page');
+  /*
+  request({
+    url: `https://www.routingnumbers.info/api/data.json?rn=321176804`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      res.send('Unable to connect to the service');
+      //console.log('Unable to connect to the service');
+    } else if (body.status === 'ZERO_RESULTS') {
+      res.send('Unable to find that address.');
+    } else {
+      res.send(`The Bank Name : ${body.customer_name.toString()}`);
+    }
+  });
+  */
+});
+
+app.get('/bad', (req, res)=> {
+  res.send({
+    errorMessage: 'unable to handle request'
+  });
+});
+
+app.listen(3000);
